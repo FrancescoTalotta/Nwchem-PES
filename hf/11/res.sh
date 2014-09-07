f18a932823f8da7859533228db3bcaed48818012
FRANCESCOTALOTTA <francesco_talotta@hotmail.com>
2014-09-07 10:45:58 -0500

    added the absolute hartree energy plot

--- hf/11/res.sh
+++ hf/11/res.sh
@@ -30,7 +30,6 @@ if [ -e "fit.log" ];then
  rm fit.log
 fi
 
-
 if [ -e "${filename}_$lastgeom.00.out" ]; then
  mv ${filename}_$lastgeom.00.out ${filename}_100.out
 fi
@@ -42,17 +41,6 @@ maxtime=0.0
 maxtimegeom=0.0
 
 for file in $(ls ${filename}_*.out | sort -V | rev |cut -c 5- |rev) ; do  #this reads from the files and extract only the name
-
-#Erase some no needed files
- if [ -e "file.db" ]; then
-  rm "$file.db"
- fi
- if [ -e "$file.moves" ]; then
-  rm "$file.movecs"
- fi
- if [ -e "$file.civec" ]; then
-  rm "$file.civec"
- fi
 
 #Check the convergence
 if grep -q 'MCSCF Converged' "$file.out";
@@ -73,128 +61,129 @@ if grep -q 'MCSCF Converged' "$file.out";
 else 
  echo "$file NOT converged" >>$out
  warning=1
-fi
-done
+ fi
+ done
 
-echo '' >> $out
-echo "Mean time is: $(echo "$total/($counter*60)" | bc) minutes, $(echo "$total/($counter)" | bc)s" >> $out
-echo "Max time is: ${maxtime}s, did by $maxtimegeom" >> $out
-echo '' >> $out
+ echo '' >> $out
+ echo "Mean time is: $(echo "$total/($counter*60)" | bc) minutes, $(echo "$total/($counter)" | bc)s" >> $out
+ echo "Max time is: ${maxtime}s, did by $maxtimegeom" >> $out
+ echo '' >> $out
 
-echo "Energy are:" >> $out
-graph=graph.dat	     #Data file, containing the distance and the energies in Hartree
-graph2=graph_kj.dat    #Data file, containing the distance and normalized energies in kcal/mol
-rif=${filename}_${lastgeom}		     #Reference file at $lastgeom Angstrom
+ echo "Energy are:" >> $out
+ graph=graph.dat	     #Data file, containing the distance and the energies in Hartree
+ graph2=graph_kj.dat    #Data file, containing the distance and normalized energies in kcal/mol
+ rif=${filename}_${lastgeom}		     #Reference file at $lastgeom Angstrom
 
-if [ -e "$rif.out" ]; then 		#check if the reference file exist
+ if [ -e "$rif.out" ]; then 		#check if the reference file exist
  rifen=$(grep 'Total MCSCF energy' "$rif.out" | cut -f2 -d"=")   #Reference energy at 100 Angstrom
-else
+ else
  echo "Can't find $rif file"
 # exit 0
-fi
+ fi
 
-cat /dev/null > $graph   #erase last data
-cat /dev/null > $graph2   #erase last data
+ cat /dev/null > $graph   #erase last data
+ cat /dev/null > $graph2   #erase last data
 
-for file in $(ls ${filename}_*.out | sort -V | rev |cut -c 5- |rev) ; do  #this reads from the files and extract only the name
+ for file in $(ls ${filename}_*.out | sort -V | rev |cut -c 5- |rev) ; do  #this reads from the files and extract only the name
 
  lengh=$(echo "$(echo ${#filename})+2" | bc)	#lengh of the character #filename
- xcoord=$(echo $file | cut -c ${lengh}-)
- tim=$(grep -E 'Total times  cpu:' "$file.out" | grep -oE '[^ ]+$')
+xcoord=$(echo $file | cut -c ${lengh}-)
+	tim=$(grep -E 'Total times  cpu:' "$file.out" | grep -oE '[^ ]+$')
 
- echo "Distance (A)"=$xcoord >> $out
- value=$(grep 'Total MCSCF energy' "$file.out")  #| sed -e 's/^[ \t]*//' >> $out   #sed, to remove black space before
- echo $value   $tim >> $out
- energy=$(grep 'Total MCSCF energy' "$file.out" | cut -f2 -d"=")
-if [ -e "$rif.out" ]; then
- ennorm=$(echo "$energy - $rifen" | bc -l)  #normalized energies. 
- enkjmol=$(echo "$ennorm*627.503" | bc -l)  #conversion from Hartree to kcal/mol\
-fi
+	echo "Distance (A)"=$xcoord >> $out
+	value=$(grep 'Total MCSCF energy' "$file.out")  #| sed -e 's/^[ \t]*//' >> $out   #sed, to remove black space before
+	echo $value   $tim >> $out
+	energy=$(grep 'Total MCSCF energy' "$file.out" | cut -f2 -d"=")
+	if [ -e "$rif.out" ]; then
+	ennorm=$(echo "$energy - $rifen" | bc -l)  #normalized energies. 
+	enkjmol=$(echo "$ennorm*627.503" | bc -l)  #conversion from Hartree to kcal/mol\
+		fi
 
- echo "${xcoord} ${energy}" >> $graph
- echo "${xcoord} ${enkjmol}" >> $graph2
+		echo "${xcoord} ${energy}" >> $graph
+		echo "${xcoord} ${enkjmol}" >> $graph2
 
- if [ $(echo "$energy < $minenergy" | bc) -eq 1 ]; then      #Search for the minimal energy
-    minenergy=$energy							 #Export it with the equilibrium distance
-    eqdist=$xcoord							 
- fi
-done
+		if [ $(echo "$energy < $minenergy" | bc) -eq 1 ]; then      #Search for the minimal energy
+		minenergy=$energy							 #Export it with the equilibrium distance
+		eqdist=$xcoord							 
+		fi
+		done
 
 #Output some usefull info into $out file
- echo '' >> $out
- echo 'Minimal parameters:' >> $out
- echo '' >> $out
- echo "Minimal Energy=$minenergy (Hartree)" | sed -e 's/^[ \t]*//' >> $out   #sed, to erase the blank spaces
- echo "Equilibrium Distance=$eqdist (Angstrom)" >> $out
+		echo '' >> $out
+		echo '-------------------------------------------------------' >> $out
+		echo 'Minimal parameters:' >> $out
+		echo '' >> $out
+		echo "Minimal Energy=$minenergy (Hartree)" | sed -e 's/^[ \t]*//' >> $out   #sed, to erase the blank spaces
+		echo "Equilibrium Distance=$eqdist (Angstrom)" >> $out
 
-if [ -e "$rif.out" ]; then
- minenkj=$(echo "scale=2;(($minenergy-$rifen)*627.503)/1" | bc -l) #Export the minimal energy in kcal/mol. The /1 is to enable the scale
-echo "Minimal Energy=$minenkj (kcal/mol)" >> $out
-fi
+		if [ -e "$rif.out" ]; then
+		minenkj=$(echo "scale=2;(($minenergy-$rifen)*627.503)/1" | bc -l) #Export the minimal energy in kcal/mol. The /1 is to enable the scale
+		echo "Minimal Energy=$minenkj (kcal/mol)" >> $out
+		fi
 
 
 #Gnuplot plot in hartree 
-      gnuplot <<- EOF
-	set terminal postscript enhanced color
-      set output "graph.ps"
-      set title "$graphtitle"
-      set  autoscale                        # scale axes automatically
-      set xrange [0:10]
-      set yrange [$minenergy-0.1:-75.0]
-      unset log                              # remove any log-scaling
-      unset label                            # remove any previous labels
-      set xtic 1.0                           # set xtics to 1 interval
-	set mxtics 2
-      set ytic auto                          # set ytics automatically
-      #set label "{${FILE}}" at 18,100
-      set encoding iso_8859_1
-      set xlabel "Distance (\305)"
-      set ylabel "Energy (a.u.)"
-      #set arrow 1 from 0.0 to 20,0 nohead
-      #set xzeroaxis
-      plot "${graph}" using 1:2 title "CASSCF Energy" with linespoints lw 2 pt 7
-EOF
+		gnuplot <<- EOF
+		set terminal postscript enhanced color
+		set output "graph.ps"
+		set title "$graphtitle"
+		set  autoscale                        # scale axes automatically
+		set xrange [0:10]
+		set yrange [$minenergy-0.1:-75.0]
+		unset log                              # remove any log-scaling
+		unset label                            # remove any previous labels
+		set xtic 1.0                           # set xtics to 1 interval
+		set mxtics 2
+		set ytic auto                          # set ytics automatically
+#set label "{${FILE}}" at 18,100
+		set encoding iso_8859_1
+		set xlabel "Distance (\305)"
+		set ylabel "Energy (a.u.)"
+#set arrow 1 from 0.0 to 20,0 nohead
+#set xzeroaxis
+		plot "${graph}" using 1:2 title "CASSCF Energy" with linespoints lw 2 pt 7
+		EOF
 
 #Gnuplot plot in kJ/mol normalized energies + polinomial fitting with x**15
-if [ -e "$rif.out" ]; then
-      gnuplot <<- EOF
-      set terminal postscript enhanced color
-      set output "graph_kj.ps"
-      set title "$graphtitle"
-      set  autoscale                        # scale axes automatically
-      set xrange [$xmin:$xmax]
-      set yrange [$minenkj-10:$ymax]
-      unset log                              # remove any log-scaling
-      unset label                            # remove any previous labels
-      set xtic 1.0                           # set xtics to 1 interval
-      set mxtics 2
-      set ytic auto                          # set ytics automatically
-      set encoding iso_8859_1
-      set label "{E_{min}= ${minenkj} (kcal/mol)}" at 1.3,-50 # plot the min energy
-      set label "{R_{m}= ${eqdist} (\305)}" at 1.3,-80	# plot the eq distance
-      set xlabel "Distance (\305)"
-      set ylabel "Energy (kcal/mol)"
-      #set arrow 1 from 0.0 to 20,0 nohead
-      set xzeroaxis
-      #f(x)=-e*((r/x)**12-2*(r/x)**6)
-	f(x)=a*x+b*x**2+c*x**3+d*x**4+e*x**5#+f*x**6+g*x**7+h*x**8+i*x**9+l*x**10+m*x**11+n*x**12+o*x**13+p*x**14+q*x**15
-      #e=$minenkj
-      #r=$eqdist
+		if [ -e "$rif.out" ]; then
+		gnuplot <<- EOF
+		set terminal postscript enhanced color
+		set output "graph_kj.ps"
+		set title "$graphtitle"
+		set  autoscale                        # scale axes automatically
+		set xrange [$xmin:$xmax]
+		set yrange [$minenkj-10:$ymax]
+		unset log                              # remove any log-scaling
+		unset label                            # remove any previous labels
+		set xtic 1.0                           # set xtics to 1 interval
+		set mxtics 2
+		set ytic auto                          # set ytics automatically
+		set encoding iso_8859_1
+		set label "{E_{min}= ${minenkj} (kcal/mol)}" at 1.3,-50 # plot the min energy
+		set label "{R_{m}= ${eqdist} (\305)}" at 1.3,-80	# plot the eq distance
+		set xlabel "Distance (\305)"
+		set ylabel "Energy (kcal/mol)"
+#set arrow 1 from 0.0 to 20,0 nohead
+	set xzeroaxis
+#f(x)=-e*((r/x)**12-2*(r/x)**6)
+	f(x)=a+b*x+c*x**2+d*x**3+e*x**4+f*x**5#+f*x**6+g*x**7+h*x**8+i*x**9+l*x**10+m*x**11+n*x**12+o*x**13+p*x**14+q*x**15
+#e=$minenkj
+#r=$eqdist
 	a=b=c=d=e=f=g=h=i=l=m=n=o=p=q=r=s=1
-      fit [0.5:1.1] f(x) '${graph2}' using 1:2 via a,b,c,d,e#,f,g,h,i,l,m,n,o,p,q
-      plot f(x) title "Fitting",  "${graph2}" using 1:2 title "CASSCF Energy" with points lw 2 pt 7
-EOF
+	fit [$eqdist-0.1:$eqdist+0.2] f(x) '${graph2}' using 1:2 via a,b,c,d,e,f#,g,h,i,l,m,n,o,p,q
+	plot f(x) title "Fitting",  "${graph2}" using 1:2 title "CASSCF Energy" with points lw 2 pt 7
+	EOF
 #evince graph_kj.ps
 #Extracting fitting parameters from gnuplot fit.log file 
-row1=$(grep -rne 'Final set of param' fit.log | cut -f1 -d":" | sed -e 's/[ \t]*$//')
-row2=$(grep -rne 'correlation matrix' fit.log | cut -f1 -d":" | sed -e 's/[ \t]*$//')
+	row1=$(grep -rne 'Final set of param' fit.log | cut -f1 -d":" | sed -e 's/[ \t]*$//')
+	row2=$(grep -rne 'correlation matrix' fit.log | cut -f1 -d":" | sed -e 's/[ \t]*$//')
 
-a=$(sed -n "$row1,${row2}p" fit.log | grep 'a[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
-b=$(sed -n "$row1,${row2}p" fit.log | grep 'b[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
-c=$(sed -n "$row1,${row2}p" fit.log | grep 'c[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
-d=$(sed -n "$row1,${row2}p" fit.log | grep 'd[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
-e=$(sed -n "$row1,${row2}p" fit.log | grep 'e[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
-#f=$(sed -n "$row1,${row2}p" fit.log | grep 'f[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
+	a=$(sed -n "$row1,${row2}p" fit.log | grep 'a[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
+	b=$(sed -n "$row1,${row2}p" fit.log | grep 'b[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
+	c=$(sed -n "$row1,${row2}p" fit.log | grep 'c[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
+	d=$(sed -n "$row1,${row2}p" fit.log | grep 'd[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
+	e=$(sed -n "$row1,${row2}p" fit.log | grep 'e[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
+	f=$(sed -n "$row1,${row2}p" fit.log | grep 'f[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
 #g=$(sed -n "$row1,${row2}p" fit.log | grep 'g[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
 #h=$(sed -n "$row1,${row2}p" fit.log | grep 'h[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
 #i=$(sed -n "$row1,${row2}p" fit.log | grep 'i[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
@@ -205,19 +194,21 @@ e=$(sed -n "$row1,${row2}p" fit.log | grep 'e[[:space:]]\{15\}' | cut -f2 -d"=" 
 #p=$(sed -n "$row1,${row2}p" fit.log | grep 'p[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
 #q=$(sed -n "$row1,${row2}p" fit.log | grep 'q[[:space:]]\{15\}' | cut -f2 -d"=" | cut -f1 -d"+" | sed -e 's/^[ \t]*//')
 
-if [ ! -e "min" ]; then
- echo 'Min program not exist'
- echo 'Stop'
- exit 0
-fi
+	if [ ! -e "min" ]; then
+	echo 'Min program not exist'
+	echo 'Stop'
+	exit 0
+	fi
 
 #Calling the min fortran program to find the minimal energy from gnuplot fitting function
-forres=$(echo -e "$a\n$b\n$c\n$d\n$e\n$eqdist" | ./min)
+	forres=$(echo -e "$a\n$b\n$c\n$d\n$e\n$f\n$eqdist" | ./min)
 
-forminen=$(echo $forres | cut -f2 -d' ')
-forminx=$(echo $forres | cut -f1 -d' ')
+	forminen=$(echo $forres | cut -f2 -d' ')
+	forminx=$(echo $forres | cut -f1 -d' ')
+	forminenhr=$(echo "scale=12;$forminen/627.503+$rifen" | bc -l) #Reconversion of energy from kcal to hartree in absolute manner
 
 #Plotting fitted equilibrium distance and energy
-echo "Equilibrium distance from fitting=$forminx (Angstrom)" >> $out
-echo "Minimal energy from fitting= $forminen (Kcal/mol)" >> $out
+	echo "Equilibrium distance from fitting=$forminx (Angstrom)" >> $out
+	echo "Minimal energy from fitting= $forminen (Kcal/mol)" >> $out
+	echo "Minimal energy from fitting (not scaled)= $forminenhr (Hartree)" >> $out
 fi
